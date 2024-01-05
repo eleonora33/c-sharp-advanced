81b99db36700708bafd670fc11d6c5e7a00a3ef2
Elleonora <eleonora_todorovska@yahoo.com>
2024-01-05 23:28:17 +0100

    Add ConsoleApp Demo

---
+++ ConsoleApp.Demos/ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("I am Eleonora Todorovska");
+
+Console.Write("Line1");
+Console.Write("Line2");
---
+++ ConsoleApp.Demos/ConsoleApp.VairablesDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name = "Eleonora Todorovska";
+
+Console.WriteLine(name);
+Console.WriteLine($"I am {name}");
+Console.WriteLine($"They call me {name}"); //interpolation
+Console.WriteLine("I am" + name); // concatenation
+Console.WriteLine("I was given the name {0}", name); //formatted string
+
+int age = 35;
+int retirementYearsLeft = 29;
+int retirementAge = age + retirementYearsLeft;
+Console.WriteLine("My age is: " + age);
+Console.WriteLine("My retirement age is: " + retirementAge);
+
+bool isRetired = false;
+Console.WriteLine("Am I retired? " +  isRetired);
+
+
+
+
