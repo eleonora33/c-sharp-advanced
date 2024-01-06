bcd03999362c11ba69d12fbcb565897bdd9a5b5d
Elleonora <eleonora_todorovska@yahoo.com>
2024-01-06 03:22:23 +0100

    Added Operators demo and enhanced user input demo.

---
+++ CSharp.Exercises/CSharp.Exercises.Random/Program.cs
@@ -0,0 +1,126 @@
+﻿//Write a C# Sharp program that takes userid and password as input (type string). After 3 wrong attempts, user will be rejected
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        string username = "";  // prvo se zadava vrednost
+//        string password = "";
+//        int attempts = 0;     // se zadava kolku pati da dozvoli sistemot da se validira
+
+//        while (attempts < 3)
+//        {
+//            Console.Write("Enter username: ");
+//            username = Console.ReadLine();
+
+//            Console.Write("Enter password: ");
+//            password = Console.ReadLine();
+
+//            if (username == "myusername" && password == "mypassword")
+//            {
+//                Console.WriteLine("Login successful!");
+//                return;
+//            }
+//            else
+//            {
+//                Console.WriteLine("Login failed. Please try again.");
+//                attempts++;
+//            }
+//        }
+
+//        Console.WriteLine("Too many failed attempts. Login rejected.");
+//    }
+//}
+
+//Write a C# Sharp program that takes two numbers as input and perform an operation
+//    (+,-,*,x,/) on them and displays the result of that operation
+
+//Console.WriteLine("Enter first number");
+//int firstNumber = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Enter operation: +, -, /, * ");
+//string operation = Console.ReadLine();
+
+//Console.WriteLine("Eneter second number:");
+//int secondNumber = int.Parse(Console.ReadLine());
+
+//int result = 0;
+
+//switch (operation)
+//{
+//    case "+":
+//        result = firstNumber + secondNumber; break;
+//    case "-":
+//        result = firstNumber - secondNumber; break;
+//    case "*":
+//        result = firstNumber * secondNumber; break;
+//    case "/":
+//        result = firstNumber / secondNumber; break;
+
+//    default:
+//        Console.WriteLine("Ivalid operation please try again");
+//        break;
+//}
+//Console.WriteLine($"The result of the operation {operation} on {firstNumber} and {secondNumber} is {result}.");
+
+//int voteAge;
+//Console.Write("\n\n");
+//Console.Write("Detrermine a specific age is eligible for casting the vote:\n");
+//Console.Write("----------------------------------------------------------");
+//Console.Write("\n\n");
+
+
+//Console.Write("Input the age of the candidate : ");
+//voteAge = Convert.ToInt32(Console.ReadLine());
+//if (voteAge < 18)
+//{
+//    Console.Write("Sorry, You are not eligible to caste your vote.\n");
+//    Console.Write("You would be able to caste your vote after {0} year.\n\n", 18 - voteAge);
+//}
+//else { 
+//    Console.Write("Congratulation! You are eligible for casting your vote.\n\n");
+//}
+
+//Write a program in C# Sharp to store elements in an array and print it.
+//var numbers = new List<int>();
+
+
+//    int inpoutNumbers = int.Parse(Console.ReadLine());
+//    numbers.Add(inpoutNumbers);
+
+//foreach (var number in numbers) 
+//{
+//    Console.WriteLine($"Numbers is: {number}");
+//}
+
+//List<int> numbers = new List<int>();
+
+//Console.WriteLine("Enter some numbers (press enter to finish):");
+
+//while (true)
+//{
+//    string input = Console.ReadLine();
+
+//    if (input == "")
+//    {
+//        break;
+//    }
+
+//    int number = int.Parse(input);
+//    numbers.Add(number);
+//}
+
+//Console.WriteLine("\nThe numbers you entered are:");
+
+//foreach (int number in numbers)
+//{
+//    Console.Write(number + " ");
+//}
+
+
+
+
+
+
+
+
---
+++ ConsoleApp.Demos/ConsoleApp.Operators/Program.cs
@@ -0,0 +1,90 @@
+﻿
+Console.WriteLine("Please enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int num2 = 0;
+Console.WriteLine("Please enter the second number: ");
+string numberEntered = Console.ReadLine();
+num2 = Convert.ToInt32(numberEntered);
+
+//Add numbers
+int sum = num1 + num2;
+
+//Multiply
+int product = num1 * num2;
+
+//Division
+int quotient = num1 / num2;
+
+//Subtraction
+int difference = num1 - num2;
+
+//Modulus
+int mod = num1 % num2;
+
+Console.WriteLine("************* Math Results ****************");
+Console.WriteLine();
+Console.WriteLine($"Sum: {sum}");
+Console.WriteLine($"Product: {product}");
+Console.WriteLine($"Quotient: {quotient}");
+Console.WriteLine($"Difference: {difference}");
+Console.WriteLine($"Modulus: {mod}");
+Console.WriteLine();
+Console.WriteLine("************* End Math Results ****************");
+
+/*
+ Logic Operations and Operators
+ */
+
+var isGreaterThan = num1 > num2;
+bool isLessThan = num1 < num2;
+bool isEqualTo = num1 == num2;
+bool isGreaterThanOrEqualTo = num1 >= num2;
+bool isLessThanOrEqualTo = num1 <= num2;
+bool isNotEqual = num1 != num2;
+Console.WriteLine();
+Console.WriteLine("************* Logic Results ****************");
+Console.WriteLine();
+Console.WriteLine($"Is Greater Than: {isGreaterThan}");
+Console.WriteLine($"Is Less Than: {isLessThan}");
+Console.WriteLine($"Is Equal To: {isGreaterThan}");
+Console.WriteLine($"Is Greater Than or Equal To: {isGreaterThanOrEqualTo}");
+Console.WriteLine($"Is Less Than or Equal To: {isLessThanOrEqualTo}");
+Console.WriteLine($"Is Not Equal: {isNotEqual}");
+Console.WriteLine();
+Console.WriteLine("************* End Logic Results ****************");
+
+
+/*
+ Assignment Operations and Operators
+ */
+//num1 = num1 + 5;
+Console.WriteLine("Enter Random Value for Assignment Operations");
+int randomValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("************* Assignment Results ****************");
+Console.WriteLine();
+//num1 += 5;
+num1 += randomValue;
+Console.WriteLine($"Num 1 increased by {randomValue}: {num1}");
+//num1 -= 3;
+num1 -= randomValue;
+Console.WriteLine($"Num 1 reduced by  {randomValue}: {num1}");
+//num1 /= 2;
+num1 /= randomValue;
+Console.WriteLine($"Num 1 divided by  {randomValue}: {num1}");
+//num1 %= 2;
+num1 %= randomValue;
+Console.WriteLine($"Num 1 mod by {randomValue}: {num1}");
+//num1 *= 10;
+num1 *= randomValue;
+Console.WriteLine($"Num 1 multiplied by {randomValue}: {num1}");
+Console.WriteLine();
+Console.WriteLine("************* End Assignment Results ****************");
+
+
+
+
+
+
+
