90d9fdeedc7aa397ffa6866fdd8bfa971ec0a60b
Elleonora <eleonora_todorovska@yahoo.com>
2024-01-07 21:51:33 +0100

    Explored Conditional and Decision statements

---
+++ ConsoleApp.Demos/ConsoleApp.ConditionsAndDecisions/Program.cs
@@ -0,0 +1,77 @@
+﻿Console.WriteLine("Please enter student grade: ");
+int grade = Convert.ToInt32(Console.ReadLine());
+
+if(grade > 50)
+{
+    Console.WriteLine("Student has passed");
+}
+else
+{
+    Console.WriteLine("Student has failed");
+    Console.WriteLine("please recommend student to student affair`s office");
+}
+
+/*
+ * A: 86-100
+ * B: 75-84
+ * C: 65-74
+ * C-:50-64
+ * F:less then 50
+ */
+
+if(grade < 0 || grade > 100)
+{
+    Console.WriteLine("Invalid value entered");
+}
+else if(grade < 50)
+{
+    Console.WriteLine("Student has failed - F");
+}
+else if(grade >= 50 && grade <= 64)
+{
+    Console.WriteLine("C-");
+}
+else if (grade >= 65 && grade <= 74)
+{
+    Console.WriteLine("C");
+}
+else if (grade >= 75 && grade <= 84)
+{
+    Console.WriteLine("B");
+}
+else if(grade >= 85 && grade <= 100)
+{
+    Console.WriteLine("A - Good job");
+}
+
+int gradeAfterBonus = grade >= 0 && grade <= 100 ? grade + 10 : 0;
+Console.WriteLine($"Grade after bonus: {gradeAfterBonus}");
+
+string passStatus = grade < 50 ? "Fail" : "Pass";
+Console.WriteLine($"Student status is: {passStatus}");
+
+Console.WriteLine("Please enter the day of the week");
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+switch (dayOfWeek)
+{
+    case 1:
+        Console.WriteLine("Sunday");
+        break;
+    case 2: Console.WriteLine("Monday");
+        break;
+    case 3: Console.WriteLine("Tuesday");
+        break;
+    case 4: Console.WriteLine("Wednesday");
+        break;
+    case 5: Console.WriteLine("Thursday");
+        break;
+    case 6: Console.WriteLine("Friday");
+        break;
+    case 7: Console.WriteLine("Saturday");
+        break;
+    default:
+        Console.WriteLine("Invalid day number entered!!!");
+        break;
+}
+
+Console.WriteLine("Thank you for using this program");
---
+++ ConsoleApp.Demos/ConsoleApp.SampleCalculator/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.WriteLine("Welcome to the sample calculator!");
+
+Console.WriteLine("Please enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please select an operation: ");
+Console.WriteLine("1.Addition");
+Console.WriteLine("2.Subtraction");
+Console.WriteLine("3.Multiplication");
+Console.WriteLine("4.Division");
+
+int choice = Convert.ToInt32(Console.ReadLine());
+
+//if (choice == 1)
+//{
+
+//}
+//else if (choice == 2)
+//{
+
+//}
+//else if (choice == 3)
+//{
+
+//}
+//else if (choice == 4)
+//{
+
+//}
+//else
+//{
+//    Console.WriteLine("Invalid choice");
+//}
+
+int answer = 0;
+switch (choice)
+{
+    case 1:
+        answer = num1 + num2;
+        break;
+    case 2:
+        answer = num1 - num2;
+        break;
+    case 3:
+        answer = num1 * num2;
+        break;
+    case 4:
+        answer = num1 / num2;
+        break;
+    default:
+        Console.WriteLine("Invalid choice");
+        break;
+}
+
+Console.WriteLine($"The result is: {answer}");
